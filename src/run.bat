656e587538758318703b022f1b19cfde26da6c4f
NormalLLer <wanqingsi1438438@gmail.com>
2025-03-31 23:32:13 +0200

    chore: change run.bat to run with segmentation

--- src/run.bat
+++ src/run.bat
@@ -1,13 +1,16 @@
 @echo off
 
-echo Running command 1:
-python -m vidore_benchmark.cli.main evaluate-retriever --model-class llava-onevision --model-name llava-hf/llava-onevision-qwen2-0.5b-ov-hf --collection-name vidore/vidore-benchmark-667173f98e70a1c0fa4db00d --dataset-format qa --split test --output-dir newOutput/
+echo Evaluating blip:
+python -m vidore_benchmark.cli.main evaluate-retriever --model-class blip --model-name Salesforce/blip-vqa-base --image-segmentation --grid-rows 2 --grid-cols 2 --collection-name vidore/vidore-benchmark-667173f98e70a1c0fa4db00d --dataset-format qa --split test --output-dir segmentedOutput/
 
-echo Running command 2:
-python -m vidore_benchmark.cli.main evaluate-retriever --model-class blip --model-name Salesforce/blip-vqa-base --collection-name vidore/vidore-benchmark-667173f98e70a1c0fa4db00d --dataset-format qa --split test --output-dir newOutput/
+echo Evaluating jina-clip-v1:
+python -m vidore_benchmark.cli.main evaluate-retriever --model-class jina-clip-v1 --model-name jinaai/jina-clip-v1 --image-segmentation --grid-rows 2 --grid-cols 2 --collection-name vidore/vidore-benchmark-667173f98e70a1c0fa4db00d --dataset-format qa --split test --output-dir segmentedOutput/
 
-echo Running command 3:
-python -m vidore_benchmark.cli.main evaluate-retriever --model-class blip --model-name Salesforce/blip2-opt-2.7b --collection-name vidore/vidore-benchmark-667173f98e70a1c0fa4db00d --dataset-format qa --split test --output-dir newOutput/
+echo Evaluating siglip:
+python -m vidore_benchmark.cli.main evaluate-retriever --model-class siglip --model-name google/siglip-so400m-patch14-384 --image-segmentation --grid-rows 2 --grid-cols 2 --collection-name vidore/vidore-benchmark-667173f98e70a1c0fa4db00d --dataset-format qa --split test --output-dir segmentedOutput/
+
+echo Evaluating llava-interleave:
+python -m vidore_benchmark.cli.main evaluate-retriever --model-class llava-interleave --model-name llava-hf/llava-interleave-qwen-0.5b-hf --image-segmentation --grid-rows 2 --grid-cols 2 --collection-name vidore/vidore-benchmark-667173f98e70a1c0fa4db00d --dataset-format qa --split test --output-dir segmentedOutput/
 
 echo All commands executed.
 pause
